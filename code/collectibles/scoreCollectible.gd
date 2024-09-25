c8ae18fd57f5a464e8d7c0b52375021ad7057e0e
D. G. Jagielski <rave002@o2.pl>
2024-09-25 20:05:38 +0200

    Added coins
    Coin. https://www.youtube.com/watch?v=7etU2lpx_mY

---
+++ code/collectibles/scoreCollectible.gd
@@ -0,0 +1,33 @@
+extends Area3D
+
+@export var scoreValue:int=10
+@export var SoundToPlay:AudioStream
+var streamPlayer: AudioStreamPlayer
+var touched:bool
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("body_entered",_on_body_entered)
+	streamPlayer = AudioStreamPlayer.new()
+	streamPlayer.stream = SoundToPlay
+	streamPlayer.pitch_scale=randf_range(0.8,1.2)
+	streamPlayer.autoplay = false
+	streamPlayer.connect("finished",streamFinished)
+	self.add_child(streamPlayer)
+	self.rotation_degrees.y = sin(self.position.x)*self.position.z*cos(self.position.y)
+
+func streamFinished():
+	if touched:
+		self.queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_body_entered(body:Node3D):
+	if body.name=="PlayerBody" and !streamPlayer.playing:
+		if (!touched):
+			Globals.score+=scoreValue
+		touched=true
+		streamPlayer.play(0.0);
+		for child in self.get_children():
+			if child is Node3D:
+				child.visible=false
+func _process(_delta)->void:
+	self.rotate_y(4*_delta)
